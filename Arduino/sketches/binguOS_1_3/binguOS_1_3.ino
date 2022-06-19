9253c16335cda5f1eda683f4693ea2796102553f
Yaseen Reza <57412770+yaseen157@users.noreply.github.com>
2022-06-19 12:33:57 +0100

    Update binguOS_1_3.ino
    Rudimentary addition of accelerometer and gyroscope compensation for lidar, as well as smart lidar starting via i2c address checking to prevent system hang

--- Arduino/sketches/binguOS_1_3/binguOS_1_3.ino
+++ Arduino/sketches/binguOS_1_3/binguOS_1_3.ino
@@ -11,8 +11,8 @@
 /* Import downloaded */
 #include <Adafruit_BMP280.h>    // Static pressure sensor (w/ temperature)
 #include <Adafruit_HTU21DF.h>   // Relative humidity sensor (w/ temperature)
-#include <Adafruit_MPU6050.h>   // 6 DoF Acc/gyro sensor (w/ temperature)
 #include <Adafruit_VL53L1X.h>   // LIDAR altimeter sensor (w/ temperature)
+#include <MPU6050_6Axis_MotionApps20.h>            // 6 DoF Acc/gyro sensor (w/ temperature)
 #include <Omron_D6FPH.h>        // Dynamic pressure sensor (w/ temperature)
 #include <SimpleKalmanFilter.h> // Uni-dimensional Kalman filtering
 #include <TinyGPSPlus.h>        // Global Positioning System
@@ -89,6 +89,7 @@ bool flag_lowbattery = false;
 bool flag_SDready = false;
 bool flag_S4fallingedge = false;
 bool flag_S4flipflop = false;
+bool flag_VL53L1Xready = false;
 unsigned long tlast_cyclestart_ms;
 unsigned long tlast_SDflushed_ms = 0;
 unsigned long tlast_SDwritten_ms = 0;
@@ -130,11 +131,13 @@ struct sensordata_RH {
   float RH_percent;       // Relative humidity measurement
   float Tpackage_C;       // Sensor package temperature
 } data_RH0;
+struct sensordata_s {
+  float distance_m;       // LiDAR time of flight measurement
+} data_s0;
 
 /* Custom classes */
 Adafruit_BMP280 bmp;
 Adafruit_HTU21DF htu;
-Adafruit_MPU6050 mpu;
 Adafruit_VL53L1X vl5;
 File datalog;
 SimpleKalmanFilter KF_D6Fq_Pa(D6F_mu, D6F_mu, D6F_pv);
@@ -142,12 +145,32 @@ LiquidCrystal lcd(
   pin_LCD_RS, pin_LCD_EN, pin_LCD_D4,
   pin_LCD_D5, pin_LCD_D6, pin_LCD_D7
   );
+MPU6050 mpu;
 Omron_D6FPH d6f;
 SoftwareSerial ss(pin_RX1, pin_TX1);
 TinyGPSPlus gps;
 
 /* Function prototypes (needed if default arguments are given) */
 // Nothing here yet!
+
+/* 6DoF sensor stuffs */
+#include "I2Cdev.h"
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 //-----------------------------------------------------------------------------
 // Setup - Run once
@@ -181,9 +204,12 @@ void setup() {
   Wire.setSDA(pin_SDA0);
   Wire.begin();
 
+  vl5begin();
   bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
   d6f.begin(MODEL_0025AD1);
   htu.begin();
+  beginMPU();
+
 
   // Save cartesian coordinates of pitot positions from polar coordinate setup
   calcPitotpositions(data_q0, data_q1, data_q2);
@@ -200,6 +226,7 @@ void loop() {
   float *battCells_V = getBatteryVoltage();
 
   // Read sensor data
+  getLIDARdistance(data_s0);
   getPitotPressure(data_q0, 0);
   getPitotPressure(data_q1, 1);
   getPitotPressure(data_q2, 2);
@@ -212,6 +239,27 @@ void loop() {
   updateIconDatalogging();
   updatePitotSolution(data_q0, data_q1, data_q2, AoA_rad, AoS_rad, q_Pa);
   updateSDreadwrite();
+
+  // IMU calculations
+  float pitch, roll;
+  float z_m = 0;
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+    // display quaternion values in easy matrix form: w x y z
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    pitch = -asin(-2.0*(q.x*q.z - q.w*q.y));
+    roll = -atan2(2.0*(q.x*q.y + q.w*q.z), q.w*q.w + q.x*q.x - q.y*q.y - q.z*q.z);
+    Serial.print("euler\t");
+    Serial.print(degrees(pitch));
+    Serial.print("\t");
+    Serial.println(degrees(roll));
+    z_m = data_s0.distance_m * cos(pitch) * cos(roll);
+  }
+  else {
+    pitch = NAN;
+    roll = NAN;
+    z_m = NAN;
+  }
 
   // Other calculations
   float Patm_Pa, Tatm_C, RH_percent;
@@ -308,7 +356,17 @@ void loop() {
       datalog.print("D6F-PH0025AD1_2 Tpackage[C] ");
       datalog.print(data_q2.Tpackage_C);
       datalog.print(", ");
-  
+
+      datalog.print("MPU6050_0 pitch[deg] ");
+      datalog.print(degrees(pitch));
+      datalog.print(", ");
+      datalog.print("MPU6050_0 roll[deg] ");
+      datalog.print(degrees(roll));
+      datalog.print(", ");
+
+      datalog.print("VL53L1X_0 s[m] ");
+      datalog.print(data_s0.distance_m);
+      datalog.print(", ");
       datalog.println(";");
     }
 
@@ -345,12 +403,18 @@ void loop() {
     lcd.write(byte(0)); // Alpha
     lcd.print(displayaoa_deg);
 
+    // Display altitude
+    char displayaltitude_m[4];
+    zeroPadFloat(displayaltitude_m, sizeof(displayaltitude_m), z_m, 1);
+    lcd.setCursor(10, 0);
+    lcd.print("z");
+    lcd.print(displayaltitude_m);
+
     // Draw extra icons
     lcd.setCursor(14, 0);
     lcd.write(byte(3)); // SD card
     lcd.write(byte(1)); // Battery
   }
-  
   
   delay(50);
 }
@@ -358,6 +422,75 @@ void loop() {
 //-----------------------------------------------------------------------------
 // Support functions
 //-----------------------------------------------------------------------------
+
+void beginMPU(void)
+{
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
+    // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
+    // the baud timing being too misaligned with processor ticks. You must use
+    // 38400 or slower in these cases, or use some kind of external separate
+    // crystal solution for the UART timer.
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+}
 
 byte * i2cscan(void)
 {
@@ -435,6 +568,18 @@ float * getBatteryVoltage(void)
   battCells_V[2] = battProbepoint3_V - battProbepoint2_V;
 
   return battCells_V;
+}
+
+
+void getLIDARdistance(sensordata_s &sd)
+{
+  if (flag_VL53L1Xready) {
+    int distance = vl5.distance();
+    sd.distance_m = (float)distance / 1000;
+  }
+  else {
+    sd.distance_m = NAN;
+  }
 }
 
 
@@ -554,7 +699,6 @@ void updateIconBattery(float battCells_V[])
   // Draw a pretty icon, starting with the empty version
   // Note: char_x's size is 8 (7 element array + terminator)
   // Note: sizeof(char_x)-n is the number of rows to be modified
-  Serial.println(battery_percent);
   for (unsigned int i = 0; i < sizeof(char_battery)-1; ++i) {
     char_battery[i] = char_battery0[i];
   }
@@ -787,4 +931,20 @@ void zeroPadFloat(char* targetarray, int targetarraysize, float float2pad, int d
   }
   // Null terminate the string (final character of the array)
   targetarray[targetarraysize-1] = '\0';
+}
+
+void vl5begin(void)
+{
+  byte *scanner = i2cscan();
+
+  flag_VL53L1Xready = false;
+  for (int i = 0; i < 128; ++i) {
+    // VL53L1X is really annoying and hangs if not present on I2C when begin is called
+    if (scanner[i] == 0x29) {
+      flag_VL53L1Xready = true;
+      vl5.begin();
+      vl5.startRanging();
+      vl5.setTimingBudget(50);
+    }
+  }
 }
